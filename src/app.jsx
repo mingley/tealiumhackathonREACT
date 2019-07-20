5463bf3ce9e5933de07e5b7092e9289c57155ac9
mikeingley <michael.ingley@gmail.com>
2019-07-20 14:59:49 -0700

    first

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import TopSpot from './topspot';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        topspots: []
+    };
+}
+
+componentWillMount(){
+  axios
+    .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+    .then(response => response.data)
+    .then(topspots => this.setState({ topspots }));
+}
+  
   render() {
     return (
-      <div className='App'></div>
+      <div className='App'>
+        
+        <div className="container">
+          <div className="jumbotron">
+          <h1 className="display-4">San Diego Top Spots</h1>
+          <p className="lead">A list of the top 30 places to see in San Diego, California.</p>
+          </div>
+            <div className='jumbotron'>
+            { 
+              this.state.topspots.map(topspot => (
+            <TopSpot
+            key={topspot.id}
+            name={topspot.name}
+            description={topspot.description}
+            location={topspot.location} /> ))
+            }
+            </div>
+        </div>
+
+      </div>
     );
   }
 }
