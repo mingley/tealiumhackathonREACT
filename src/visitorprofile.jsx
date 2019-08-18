cb8591e582bf34badbe7680e60b925d1b6b6e26a
taylour <jayluvsdivn@gmail.com>
2019-08-18 15:28:04 -0700

    re-adds '$' to USD values

--- src/visitorprofile.jsx
+++ src/visitorprofile.jsx
@@ -9,9 +9,9 @@ export default props => (
       <h5 className='text-primary'>Metrics</h5>
       <a className='btn btn-primary btn-sm' 
           href={'https://maps.google.com/?q='+props.metrics['lat']+','+props.metrics['long']}>View your location</a>
-      <p>{props.metrics['Average Purchase Value'] ? "Average Purchase Value(USD): " + (Math.round(props.metrics['Average Purchase Value'] * 100) / 100) : null}</p>
-      <p>{props.metrics['Average Value Per Visit'] ? "Average Value Per Visit(USD): " + (Math.round(props.metrics['Average Value Per Visit'] * 100) / 100) : null}</p>
-      <p>{props.metrics['Lifetime purchases'] ? "Lifetime Purchases(USD): " + (Math.round(props.metrics['Lifetime purchases'] * 100) / 100) : null}</p>
+      <p>{props.metrics['Average Purchase Value'] ? "Average Purchase Value(USD): $" + (Math.round(props.metrics['Average Purchase Value'] * 100) / 100) : null}</p>
+      <p>{props.metrics['Average Value Per Visit'] ? "Average Value Per Visit(USD): $" + (Math.round(props.metrics['Average Value Per Visit'] * 100) / 100) : null}</p>
+      <p>{props.metrics['Lifetime purchases'] ? "Lifetime Purchases(USD): $" + (Math.round(props.metrics['Lifetime purchases'] * 100) / 100) : null}</p>
       <p>{props.metrics['Lifetime visits'] ? "Lifetime Visits: " + props.metrics['Lifetime visits'] : null}</p>
       <p>{props.metrics['Total time spent on site in minutes'] ? "Time spent on site(Minutes): " + props.metrics['Total time spent on site in minutes'] : null}</p>
       <p>{props.metrics['Number of logins - Past 90 days'] ? "Number of logins in the past 90 days: " + props.metrics['Number of logins - Past 90 days'] : null}</p>
